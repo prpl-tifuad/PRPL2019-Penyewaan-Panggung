53c4d76d9283c3bd7d996d77fd6c8e9379f1cfa6
Rizki Akbari <40619449+RizkiAkbari@users.noreply.github.com>
2019-04-12 22:39:36 +0800

    Update input.php

--- input.php
+++ input.php
@@ -2,31 +2,47 @@
 <!DOCTYPE html>
 <html>
 <head>
+	
 	<title></title>
+	
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<script src="js/jquery-3.3.1.slim.min.js"></script>
+    <script src="popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 </head>
 <body>
 
 <h3>Form Input Data</h3>
 
 <form method="post" action="">
-	<table>
-		<tr>
-			<td>Nama</td>
-			<td><input type="text" name="nama"></td>
-		</tr>
-		<tr>
-			<td>Harga</td>
-			<td><input type="text" name="harga"></td>
-		</tr>
-		<tr>
-			<td>Ukuran</td>
-			<td><input type="text" name="ukuran"></td>
-		</tr>
-		<tr>
-			<td></td>
-			<td><input type="submit" value="Simpan"></td>
-		</tr>
-	</table>
+	<div class="container">
+		<h5 class="display-4 text-center py-5">FORM INPUT</h5>
+		<div class="row">
+			<div class="col-2"></div>
+			<div class="col-8">
+				<form action="inputkaryawan.php" method="post">
+				  <div class="form-group">
+				    <label for="id_karyawan">Kode</label>
+				    <input type="text" class="form-control" id="kode" name="kode" placeholder="Kode">
+				  </div>
+				  <div class="form-group">
+				    <label for="nama_karyawan">Nama</label>
+				    <input type="text" class="form-control" id="nama" name="nama" placeholder="Nama">
+				  </div>
+				  <div class="form-group">
+				    <label for="no_tlp">Harga</label>
+				    <input type="text" class="form-control" id="harga" name="harga" placeholder="Harga">
+				  </div>
+				  <div class="form-group">
+				    <label for="alamat">Ukuran</label>
+				    <input type="text" class="form-control" id="ukuran" name="ukuran" placeholder="Ukuran">
+				  </div>
+				  <button type="submit" class="btn btn-danger">Submit</button>
+				</form>
+			</div>
+			<div class="col-2"></div>
+		</div>
+	</div>
 </form>
 
 <?php
